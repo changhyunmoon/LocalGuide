ed14e3440a045cd1bc8f4cc1e5b9a13b948b520e
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-03-25 21:41:45 +0900

    Feat/be#10: GlobalException 구현

--- backend/src/main/java/com/team6/backend/global/excpetion/ExceptionCode.java
+++ backend/src/main/java/com/team6/backend/global/excpetion/ExceptionCode.java
@@ -1,4 +1,13 @@
 package com.team6.backend.global.excpetion;
 
-public class ExceptionCodel {
+import org.springframework.http.HttpStatus;
+
+public interface ExceptionCode {
+
+    HttpStatus getStatus();
+
+    String getCode();
+
+    String getMessage();
+
 }
