68339d923e20b52c49b0305f0839b681b56c6711
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-04-01 10:44:05 +0900

    temp

--- backend/module-chat/src/main/java/com/team6/module/chat/domain/ChatRoom.java
+++ backend/module-chat/src/main/java/com/team6/module/chat/domain/ChatRoom.java
@@ -1,4 +1,24 @@
 package com.team6.module.chat.domain;
 
-public class ChatRoom {
+import com.team6.module.common.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Table(name = "chat_room")
+@NoArgsConstructor
+public class ChatRoom extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+
 }
